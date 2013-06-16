d8e87b84c392028dc119aba50439fba98eba797c
Adriel Service <adriel.service@gmail.com>
2013-06-16 02:20:19 -0400

    DB/Initial Draft

---
+++ Dresseria/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Dresseria
+//
+//  Created by David Beilis on 6/15/2013.
+//  Copyright (c) 2013 Adriel Service. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DresseriaTests/DresseriaTests.m
@@ -0,0 +1,36 @@
+//
+//  DresseriaTests.m
+//  DresseriaTests
+//
+//  Created by David Beilis on 6/15/2013.
+//  Copyright (c) 2013 Adriel Service. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DresseriaTests : XCTestCase
+
+@end
+
+@implementation DresseriaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
